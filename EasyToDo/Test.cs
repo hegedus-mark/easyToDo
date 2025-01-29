0dec06206075da490ff061149bafb999654b9a4e
hegedus-mark <hegedusmark2018@gmail.com>
2025-01-29 10:31:42 +0100

    init

---
+++ EasyToDo/Test.cs
@@ -0,0 +1,14 @@
+namespace EasyToDo;
+
+public class Test
+{
+    public Test(string name, int count)
+    {
+        Name = name;
+        Count = count;
+    }
+
+    public string Name { get; set; }
+    public int Count { get; set; }
+
+}
